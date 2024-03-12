152fc45e6a2730695b154a992124c180d851fd00
ci <ci@hail.is>
2024-03-12 20:18:24 +0000

    setup repo

---
+++ hail/python/hail/docs/_static/hail_version.js
@@ -0,0 +1,1 @@
+hail_version="0.2.128-3ce86980f7d5";hail_pip_version="0.2.128"
---
+++ hail/python/hail/genetics/__init__.py
@@ -0,0 +1,7 @@
+from .allele_type import AlleleType
+from .call import Call
+from .reference_genome import ReferenceGenome
+from .pedigree import Pedigree, Trio
+from .locus import Locus
+
+__all__ = ['AlleleType', 'Locus', 'Call', 'Pedigree', 'Trio', 'ReferenceGenome']
